20440529b17af4ed4577ef9ad5c0a3fbaf7b0696
vishal16052006-design <vishal16052006@gmail.com>
2026-09-18 22:32:49 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Smart Bio-Reactor",
+    page_icon="🧪",
+    layout="wide"
+)
+
+st.title("Smart Bio-Reactor")
+
+st.write("Bio-reactor monitoring system")
